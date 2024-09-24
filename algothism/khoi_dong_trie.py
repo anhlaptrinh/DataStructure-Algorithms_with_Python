edb825266c2ef682bdf2a8e6607ed4c1eaffb116
Lê Quang Ánh <superanhziggy@gmail.com>
2024-09-24 19:51:28 +0900

    bài tập 8: khởi động trie

---
+++ algothism/khoi_dong_trie.py
@@ -0,0 +1,7 @@
+# Dữ liệu đầu vào
+product = ["cat", "banana", "obama", "car", "cow", "alibaba"]
+
+
+for index, value in enumerate(product):
+    if value.startswith("ba"):
+        print(f'Phần tử "{value}" có chỉ số: {index}')

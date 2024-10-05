ab40d7d41a647640cf08843be246bf33d3fc5bb5
Lê Quang Ánh <superanhziggy@gmail.com>
2024-10-05 17:16:06 +0900

    binary tree

---
+++ algothism/binary_tree.py
@@ -0,0 +1,80 @@
+root = {
+    "value": 10,
+    "left": {
+        "value": 7,
+        "left": {
+            "value": 2,
+            "left": {
+                "value": 1,
+                "left": {},
+                "right": {}
+            },
+            "right": {
+                "value": 3,
+                "left": {},
+                "right": {}
+            }
+        },
+        "right": {
+            "value": 9,
+            "left": {},
+            "right": {}
+        }
+    },
+    "right": {
+        "value": 84,
+        "left": {
+            "value": 20,
+            "left": {},
+            "right": {
+                "value": 66,
+                "left": {
+                    "value": 44,
+                    "left": {},
+                    "right": {
+                        "value": 55,
+                        "left": {},
+                        "right": {}
+                    }
+                },
+                "right": {}
+            }
+        },
+        "right": {
+            "value": 100,
+            "left": {},
+            "right": {}
+        }
+    }
+}
+
+
+price = {
+    "value": 13,
+    "left": {
+        "value": 4,
+        "left": {
+            "value": 2,
+            "left": {},
+            "right": {}
+        },
+        "right": {
+            "value": 5,
+            "left": {},
+            "right": {}
+        }
+    },
+    "right": {
+        "value": 20,
+        "left": {
+            "value": 14,
+            "left": {},
+            "right": {}
+        },
+        "right": {
+            "value": 22,
+            "left": {},
+            "right": {}
+        }
+    }
+}
